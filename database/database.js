e820a5cf219987e69309a9d3bd8fc4f0a7667e66
Mírian <mirianlima0514@gmail.com>
2024-07-26 14:08:41 +0100

    mudando para variaveis de ambiente

--- database/database.js
+++ database/database.js
@@ -1,13 +1,14 @@
 import knex from "knex";
-
+import dotenv from "dotenv"
+dotenv.config()
 
 export default knex({
   client: "mysql2",
   connection: {
-    host: "localhost",
-    port: 3306,
-    database: "faturacao",
-    user: "root",
+    host: process.env.HOST,
+    port: process.env.PORT,
+    database: process.env.DATABASE,
+    user: process.env.USER,
     password: "", // Agora é uma string
   },
   pool: { min: 0, max: 10 },
