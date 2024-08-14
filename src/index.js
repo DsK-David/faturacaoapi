f5c192958d1f6d0b16d1b8b1aa35b999af5245de
David silva <89320886+DsK-David@users.noreply.github.com>
2024-08-14 16:05:28 -0100

    criação da rota para mandar toda as vendas e a função de gerar faturas em formato PDF

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import express from "express";
 import dotenv from "dotenv"
 dotenv.config()
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 import { createServer } from "http";
 // Importação de funções para interagir com o banco de dados
 import {
@@ -14,35 +14,126 @@ import mostrarTodoProduto from "../controller/mostrarTodoProduto.js";
 import { entidade } from "../repositories/entidade.js";
 import { auth } from "../repositories/login.js";
 import {
-  mostrarProdutoPeloNome,
+  mostrarProdutoPeloNome,  
   mostrarProdutoPorBarCode,
   mostrarProdutoPorCategoria,
   mostrarProdutoPorEntidade,
 } from "../repositories/produto.js";
 import mostrarTodaCategoria from "../controller/mostrarTodaCategoria.js";
 import { mostrarCategoriaPorEntidade } from "../repositories/categorias.js";
-import { mostrarTodaVendaPorEntidade } from "../repositories/vendas.js";
+import { adicionarVenda, mostrarTodaVendaPorEntidade } from "../repositories/vendas.js";
+import mostrarTodaVenda from "../controller/mostrarTodaVenda.js";
+import path from "path";
+import ejs from "ejs";
+import pdf from "html-pdf";
+import fs from "fs";
+
 
 // Criação da instância do aplicativo Express
 const app = express();
 const server = createServer(app);
-function generateApiKey(){
-  return uuidv4()
-}
+app.set("view engine", "ejs");
+import { fileURLToPath } from "url";
+
+// Obter o diretório atual usando import.meta.url
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Configuração do caminho para as views
+app.set("views", path.join(__dirname, "views"));
+
+// function generateApiKey(){
+//   return uuidv4()
+// }
 function verifyApiKey(req,res,next){
   const apikey = req.headers["authorization"]
   const validApiKey=process.env.APIKEY
+
 
   if(!apikey || apikey !== validApiKey){
     return res.status(401).json({message:"a chave da api esta faltando"})
   }
   next()
 }
+async function teste(venda,res){
+  venda.Itens_Comprados.forEach((item) => {
+    res.send(item.quantidade)
+  });
+}
+//gerador de fatura automatica
+export async function gerarFaturaPDF(venda, res) {
+const invoiceData = {
+  id: venda.Venda_ID,
+  nomeCliente: venda.nomeCliente ?? "Cliente Teste",
+  data: new Date(venda.Data_Venda),
+  valorTotal: parseFloat(venda.Valor_Total) || 0,
+  itens: [],
+};
+
+venda.Itens_Comprados.forEach((item) => {
+  invoiceData.itens.push({
+    precoUni:parseFloat( item.productPrice),
+    quantidade: parseFloat(item.quantity) || 0,
+    total: parseFloat(item.total) || 0,
+  });
+});
 
 
+
+
+
+  try {
+    const html = await ejs.renderFile(
+      path.join(__dirname, "views", "invoice-template.ejs"),
+      { invoice: invoiceData }
+    );
+
+    const options = {
+      format: "A5",
+      border: {
+        top: "2mm",
+        right: "2mm",
+        bottom: "2mm",
+        left: "2mm",
+      },
+      footer: {
+        height: "8mm",
+        contents: {
+          default:
+            '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>',
+        },
+      },
+    };
+
+    pdf.create(html, options).toStream((err, stream) => {
+      if (err) {
+        console.error("Erro ao criar o PDF:", err);
+        if (!res.headersSent) {
+          res.status(500).send("Erro ao gerar o PDF");
+        }
+        return;
+      }
+
+      if (!res.headersSent) {
+        res.setHeader(
+          "Content-disposition",
+          `attachment; filename="Fatura.pdf"`
+        );
+        res.setHeader("Content-type", "application/pdf");
+      }
+
+      stream.pipe(res);
+    });
+  } catch (error) {
+    console.error("Erro ao gerar o PDF:", error);
+    if (!res.headersSent) {
+      res.status(500).send("Erro ao gerar o PDF");
+    }
+  }
+}
 // Configuração do middleware para analisar corpos de requisições HTTP com formato JSON
 app.use(express.json());
-
+console.log(process.env.APIKEY)
 
 
 // Rota raiz que retorna uma mensagem simples "Hello World!"
@@ -52,7 +143,7 @@ app.get("/", (req, res) => {
 
 // Endpoint para retornar todos os clientes
 app.get("/api/v1/cliente",verifyApiKey, mostrarTodoCliente);
-
+app.get("/api/v1/vendas",mostrarTodaVenda)
 // Endpoint para buscar todos os clientes de uma entidade 
 app.get("/api/v1/cliente/:entidadeID", async (req, res) => {
   const { entidadeID } = req.params;
@@ -79,7 +170,7 @@ app.get("/api/v1/produto/:entidadeID", async (req, res) => {
 });
 
 // Endpoint para buscar um produto pelo seu código de barras
-app.get("/api/v1/produto/barcode/:barcode", async (req, res) => {
+app.get("/api/v1/produto/barcode/:barcode",verifyApiKey, async (req, res) => {
   const { barcode } = req.params;
   try {
     const produto = await mostrarProdutoPorBarCode(barcode);
@@ -97,7 +188,7 @@ app.get("/api/v1/produto/barcode/:barcode", async (req, res) => {
 
 
 // Endpoint para buscar um produto pelo nome
-app.get("/api/v1/produto/nome/:produto_nome/:entidadeID", async (req, res) => {
+app.get("/api/v1/produto/nome/:produto_nome/:entidadeID",verifyApiKey, async (req, res) => {
   const { produto_nome,entidadeID } = req.params;
   try {
     const produto = await mostrarProdutoPeloNome(produto_nome,entidadeID);
@@ -109,7 +200,7 @@ app.get("/api/v1/produto/nome/:produto_nome/:entidadeID", async (req, res) => {
     res.status(500).send({ error: error.message });
   }
 });
-app.get("/api/v1/produto/categoria/:categoriaID", async (req, res) => {
+app.get("/api/v1/produto/categoria/:categoriaID",verifyApiKey, async (req, res) => {
   const { categoriaID } = req.params;
   try {
     const produto = await mostrarProdutoPorCategoria(categoriaID);
@@ -123,7 +214,7 @@ app.get("/api/v1/produto/categoria/:categoriaID", async (req, res) => {
 });
 
 // Endpoint para autenticar um usuário com base no nome de usuário e senha
-app.get("/api/v1/auth/:username/:password", async (req, res) => {
+app.get("/api/v1/auth/:username/:password",verifyApiKey, async (req, res) => {
   const { username, password } = req.params;
   try {
     const user = await auth(username, password);
@@ -139,7 +230,7 @@ app.get("/api/v1/auth/:username/:password", async (req, res) => {
 });
 
 // Endpoint para buscar informações sobre uma entidade específica pelo ID
-app.get("/api/v1/entidade/:id", async (req, res) => {
+app.get("/api/v1/entidade/:id",verifyApiKey, async (req, res) => {
   const { id } = req.params;
   try {
     const user = await entidade(id);
@@ -153,7 +244,7 @@ app.get("/api/v1/entidade/:id", async (req, res) => {
 });
 
 // Endpoint para retornar todas as categorias
-app.get("/api/v1/categoria", mostrarTodaCategoria);
+app.get("/api/v1/categoria", verifyApiKey,mostrarTodaCategoria);
 
 // Endpoint para buscar uma categoria específica pelo ID
 app.get("/api/v1/categoria/:id", async (req, res) => {
@@ -170,7 +261,7 @@ app.get("/api/v1/categoria/:id", async (req, res) => {
 });
 
 // Endpoint para buscar todas as vendas associadas a uma entidade específica pelo ID
-app.get("/api/v1/vendas/:id", async (req, res) => {
+app.get("/api/v1/vendas/:id",verifyApiKey, async (req, res) => {
   const { id } = req.params;
   try {
     const vendas = await mostrarTodaVendaPorEntidade(id);
@@ -180,6 +271,25 @@ app.get("/api/v1/vendas/:id", async (req, res) => {
     res.send(vendas);
   } catch (error) {
     res.status(500).send({ error: error.message });
+  }
+});
+app.post("/api/v1/venda/", async (req, res) => {
+  try {
+    const { Entidade_ID, UTILIZADOR, Itens_Comprados, Valor_Total } = req.body;
+
+    if (!Entidade_ID || !UTILIZADOR || !Itens_Comprados || !Valor_Total) {
+      return res.status(400).json({ error: "Dados da venda incompletos" });
+    }
+
+    const venda = await adicionarVenda(Entidade_ID, UTILIZADOR, Itens_Comprados, Valor_Total);
+    // res.send(venda)
+    await gerarFaturaPDF(venda,res);
+
+   
+    // res.send(venda)
+  } catch (error) {
+    console.error("Erro ao registrar venda:", error);
+    res.status(500).json({ error: "Erro ao registrar venda" });
   }
 });
 
