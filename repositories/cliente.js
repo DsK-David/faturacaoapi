35e1fedb8215b5a4ab00011aead48e62a95c8939
David Silva <fbi.gov2015@gmail.comm>
2024-10-10 16:48:56 -0100

    modificando a função de adicionar cliente a base de dados colocando mais informações sobre o cliente informações verdadeiras

--- repositories/cliente.js
+++ repositories/cliente.js
@@ -6,13 +6,47 @@ export async function mostrarClientes() {
 export async function mostrarClientePorEntidade(entidadeID){
   return database.select().from("cliente").where("Entidade_ID",entidadeID)
 }
-export async function adicionarClientePorEntidade(IND_COLETIVO,DESIG,DESCR,NIF,NUM_CLIENTE,EMAIL,TELEFONE,Entidade_ID){
-  return database.insert({IND_COLETIVO,DESIG,DESCR,NIF,NUM_CLIENTE,EMAIL,TELEFONE,Entidade_ID}).into("cliente_teste")
+export async function adicionarClientePorEntidade(
+  ID,
+  CODIGO,
+  IND_COLETIVO,
+  DESIG,
+  DESCR,
+  NIF,
+  NUM_CLIENTE,
+  EMAIL,
+  TELEFONE,
+  ENDERECO,
+  DT_REGISTO,
+  DT_ALTERACAO,
+  ESTADO,
+  glb_user_ID,
+  Entidade_ID
+) {
+  return database
+    .insert({
+      ID,
+      CODIGO,
+      IND_COLETIVO,
+      DESIG,
+      DESCR,
+      NIF,
+      NUM_CLIENTE,
+      EMAIL,
+      TELEFONE,
+      ENDERECO,
+      DT_REGISTO,
+      DT_ALTERACAO,
+      ESTADO,
+      glb_user_ID,
+      Entidade_ID,
+    })
+    .into("cliente");
 }
 export async function deletarClientePorEntidade(clienteID,entidadeID){
 return database
   .delete()
-  .from("cliente_teste")
+  .from("cliente")
   .where("ID", clienteID)
   .andWhere("Entidade_ID", entidadeID);
 }
