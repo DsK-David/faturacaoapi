ad81f31b604e3f7f10c9576f79599bc2c65e8718
David Silva <fbi.gov2015@gmail.comm>
2024-10-17 16:40:23 -0100

    adicionando resposta padrão para cada rota

--- controller/mostrarTodoProduto.js
+++ controller/mostrarTodoProduto.js
@@ -2,8 +2,12 @@ import { mostrarProduto } from "../repositories/produto.js";
 export default async function (req,res){
     try {
         const resultado = await mostrarProduto()
-        res.status(200).send(resultado)
+        res
+          .status(200)
+          .json(respostaPadrao(true, "Operação bem sucedida", resultado));
     } catch (error) {
-        
+        res
+          .status(500)
+          .json(respostaPadrao(false, "Operação mal sucedida", error));
     }
 }
